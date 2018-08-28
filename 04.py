42c78fa6a96e7bf0beee363b78cfc6c0ed47d15c
山田暉 <hikaruy0805@gmail.com>
2018-08-28 20:20:56 +0900

    almost ok, but not understood result[word[0:1]] = num

--- 04.py
+++ 04.py
@@ -5,11 +5,10 @@ words = nltk.word_tokenize(elements) #単語分割
 num_first_only = (1, 5, 6, 7, 8, 9, 15, 16, 19)
 result = {}
 
-for num, words in enumerate(words):
-  if num == num_first_only:
-    result[words[0:1]] = num
+for num, word in enumerate(words, 1):
+  if num in num_first_only:
+    result[word[0:1]] = num
   else:
-    result[words[0:2]] = num
+    result[word[0:2]] = num
 
-print(result)
-
+print(result, num)
