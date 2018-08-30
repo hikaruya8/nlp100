1d44dbe0842b1f44e517769688abd8af58cf9a6f
山田暉 <hikaruy0805@gmail.com>
2018-08-30 18:09:38 +0900

    場合分けは出来てる。shuffleする位置だけやる

---
+++ 09.py
@@ -0,0 +1,25 @@
+# 09. Typoglycemia
+# スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ
+
+import nltk
+import random
+
+def typoglycemia(document):
+  result = ''
+  doc = document
+  doc_tokens = nltk.word_tokenize(doc)
+  for t in doc_tokens:
+    if len(t) <= 4: #単語の長さが４文字以下の場合そのまま出力
+      result += t
+    else:           #単語の長さが５以上ならば
+      print(t)
+      # for a in t:
+      #   t += random.shuffle(a)
+      # result += t
+      # result += t[0] + random.shuffle(t[1:-1])+ t[-1] #先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替える
+
+
+document = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+typoglycemia(document)
+
+
