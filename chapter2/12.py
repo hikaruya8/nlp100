699bc0e802489d795d86417acfe275c1a9a8ac6c
山田暉 <hikaruy0805@gmail.com>
2018-09-04 10:09:46 +0900

    sublime textおかしい

--- chapter2/12.py
+++ chapter2/12.py
@@ -7,16 +7,23 @@ import numpy as np #numpyで2次元配列にする
 f = open('hightemp.txt')
 document = f.read()
 doc_token = nltk.word_tokenize(document)
-doc_2d = np.array(doc_token).reshape((24,4))
-print(doc_2d)
+doc_2d = np.array(doc_token).reshape((24,4)) #24行4列 ndarrayにする
 
-# def tab_space(document):
-#   d_list = ''
-#   for doc in document:
-#     d = doc.expandtabs(1)
-#     d_list += d
-#   return d_list
+col1_txt = doc_2d[:,0] #各行の1列目だけを抜き出したもの
+col2_txt = doc_2d[:,1] #各行の1列目だけを抜き出したもの
 
+
+path_w = '/Users/yamadahikaru/Projects/ML_Projects_Python/nlp100/chapter2/12.py'
+s = 'col1_txt'
+
+with open(path_w, mode='w') as f:
+    f.write(s)
+
+
+# New file
+
+print(f)
+print(col2_txt)
 # def split_row(document):
 #   d_list = []
 #   for d in document:
