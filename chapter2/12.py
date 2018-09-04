3574debb4eb34158b7c9e3d6e24aedd6ed29cc70
山田暉 <hikaruy0805@gmail.com>
2018-09-04 09:45:43 +0900

    配列にはなった。numpyで2次元配列にする

--- chapter2/12.py
+++ chapter2/12.py
@@ -1,20 +1,29 @@
 # 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
 # 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
 
-import numpy as np
+import nltk
+import numpy as np #numpyで2次元配列にする
 
-document = open('hightemp.txt')
+f = open('hightemp.txt')
+document = f.read()
+doc_token = nltk.word_tokenize(document)
 
-def tab_space(document):
-  d_list = ''
-  for doc in document:
-    d = doc.expandtabs(1)
+# def tab_space(document):
+#   d_list = ''
+#   for doc in document:
+#     d = doc.expandtabs(1)
+#     d_list += d
+#   return d_list
+
+def split_row(document):
+  d_list = []
+  for d in document:
     d_list += d
   return d_list
 
-doc_list = tab_space(document)
-print(doc_list)
+
+print(split_row(document))
 
 
-# def split_row():
+
 
