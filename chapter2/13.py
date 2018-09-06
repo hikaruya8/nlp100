018e75de4f0ecc6a06e3b77d73f3e2d1fb8d9054
山田暉 <hikaruy0805@gmail.com>
2018-09-06 09:33:14 +0900

    もう一回やる必要あり

--- chapter2/13.py
+++ chapter2/13.py
@@ -1,9 +1,16 @@
 # 13. col1.txtとcol2.txtをマージ
 # 12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
 
-import glob
-import subprocess
+# import glob
+# import subprocess
 
-for f in glob.glob("col*.txt"):
-  subprocess.call("cat "+f+" >> OutFile.txt", shell=True)
+# for f in glob.glob("col*.txt"):
+#   subprocess.call("cat "+f+" >> OutFile.txt", shell=True)
+# 上は羅列だけになってる
 
+with open("col1.txt") as col1_file, \
+     open("col2.txt") as col2_file, \
+     open("merge.txt", mode="w") as out_file:
+
+     for col1_line, col2_line in zip(col1_file, col2_file):
+        out_file.write(col1_line.rstrip() + '\t' + col2_line.rstrip() + '\n')
