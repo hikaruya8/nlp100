3632f4d1c03cce48e725049fc268e085312f415b
山田暉 <hikaruy0805@gmail.com>
2018-09-07 10:21:20 +0900

    15 complete

---
+++ chapter2/15.py
@@ -0,0 +1,25 @@
+# 15. 末尾のN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ
+
+import nltk
+import numpy as np #numpyで2次元配列にする
+
+def make_col_n(n):
+  f = open('hightemp.txt')
+  document = f.read()
+  doc_token = nltk.word_tokenize(document)
+  doc_2d = np.array(doc_token).reshape((24,4)) #24行4列 ndarrayにする
+
+  row_n = doc_2d[(-int(n)):,:] #1~n行目を抜き出したもの
+
+
+  path_w = '/Users/yamadahikaru/Projects/ML_Projects_Python/nlp100/chapter2/row_n.txt'
+
+  for l in row_n:
+    with open(path_w, mode='w') as f:
+      f.write(' '.join(l))
+    with open(path_w) as f:
+      print(f.read())
+
+n = input("自然数(半角数字)を入力してください: ")
+make_col_n(n)
