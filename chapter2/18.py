2c8665f94cfb98dd126eaabd2a9418a92082afb3
山田暉 <hikaruy0805@gmail.com>
2018-09-10 07:38:46 +0900

    指定列でsortはできたけど、行の順番変わっちゃう

--- chapter2/18.py
+++ chapter2/18.py
@@ -1,3 +1,14 @@
 # 18. 各行を3コラム目の数値の降順にソート
 # 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
 
+import nltk
+import numpy as np #numpyで2次元配列にする
+
+def sort_row3():
+  f = open('hightemp.txt')
+  document = f.read()
+  doc_token = nltk.word_tokenize(document)
+  doc_2d = np.array(doc_token).reshape((24,4)) #24行4列 ndarrayにする
+  print(doc_2d[:, doc_2d[2].argsort(axis=0, kind='quicksort')][::-1])
+
+sort_row3()
