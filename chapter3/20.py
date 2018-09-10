719ce2c78f46e00bb75b39583d4745b5dd6b866c
山田暉 <hikaruy0805@gmail.com>
2018-09-11 07:37:05 +0900

    ok

--- chapter3/20.py
+++ chapter3/20.py
@@ -1,3 +1,12 @@
 # 20. JSONデータの読み込み
 # Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
 
+import gzip
+import json
+
+with gzip.open('jawiki-country.json.gz', 'rt') as data:
+  for line in data:
+    data_json = json.loads(line)
+    if data_json['title'] == 'イギリス':
+      print(data_json['text'])
+      break
