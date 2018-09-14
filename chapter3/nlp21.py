4a532c8017825b843a0d3edda36580876aef9010
山田暉 <hikaruy0805@gmail.com>
2018-09-14 18:37:52 +0900

    できた

--- chapter3/nlp21.py
+++ chapter3/nlp21.py
@@ -1,20 +1,10 @@
 # 21. カテゴリ名を含む行を抽出
 # 記事中でカテゴリ名を宣言している行を抽出せよ．
 
-import pandas as pd
-import numpy as np
-import re
 import nlp20
 
-# data = pd.read_json('/Users/yamadahikaru/Projects/ML_Projects_Python/nlp100/chapter3/jawiki-country.json.gz', lines=True)
-# rown = data.text.loc[1]#pandas.read_jsonのオプションを読む
-# # Category_row = rown.loc[]
-
-data_uk = nlp20.read_uk('text')
-itr_data_uk = re.finditer('Category', data_uk)
-for i in itr_data_uk:
-  print(i)
-
-
-
+data_uk = nlp20.read_uk('text').splitlines()
+for d in data_uk:
+  if 'Category' in d:
+    print(d)
 
