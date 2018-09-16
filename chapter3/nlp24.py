baa9dc3d7ff852df7ba1b1aac77bb55ce065f29b
山田暉 <hikaruy0805@gmail.com>
2018-09-17 04:36:47 +0900

    need re.compile

--- chapter3/nlp24.py
+++ chapter3/nlp24.py
@@ -1,3 +1,15 @@
 # 24. ファイル参照の抽出
 # 記事から参照されているメディアファイルをすべて抜き出せ．
+import nlp20
+import re
+
+data_uk = nlp20.read_uk('text').splitlines()
+pattern1 = r'ファイル:'
+pattern2 = r'File:'
+
+for line in data_uk:
+  matchmd1 = re.search(pattern1, line)
+  matchmd2 = re.search(pattern2, line)
+  if matchmd1 or matchmd2:
+    print(line)
 
