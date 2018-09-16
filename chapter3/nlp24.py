793315e0a6fc49dc2032e9cf012d631f7d935a53
山田暉 <hikaruy0805@gmail.com>
2018-09-17 04:53:28 +0900

    almost understood

--- chapter3/nlp24.py
+++ chapter3/nlp24.py
@@ -3,13 +3,16 @@
 import nlp20
 import re
 
-data_uk = nlp20.read_uk('text').splitlines()
-pattern1 = r'ファイル:'
-pattern2 = r'File:'
+data_uk = nlp20.read_uk('text')
 
-for line in data_uk:
-  matchmd1 = re.search(pattern1, line)
-  matchmd2 = re.search(pattern2, line)
-  if matchmd1 or matchmd2:
-    print(line)
+pattern = re.compile(r'''
+  (?:File|ファイル)
+  :
+  (.+?)
+  \|
+  ''', re.VERBOSE)
+
+matchmd = pattern.findall(data_uk)
+for line in matchmd:
+  print(line)
 
