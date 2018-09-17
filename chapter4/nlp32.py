6fe55add10ebd3961777eaf24c5aa0bd3556cd32
山田暉 <hikaruy0805@gmail.com>
2018-09-17 22:13:28 +0900

    31のsurfaceをbaseにしただけ

---
+++ chapter4/nlp32.py
@@ -0,0 +1,15 @@
+# 32. 動詞の原形
+# 動詞の原形をすべて抽出せよ
+
+import nlp30
+
+nlp30.parse_neko()
+
+lines = nlp30.neko_lines()
+verbs = []
+for line in lines:
+  for morpheme in line:
+    if morpheme['pos'] == '動詞':
+      verbs.append(morpheme['base'])
+
+print(verbs)
