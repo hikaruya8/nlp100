8fe75a668193f523147ac35685d61d386f58b596
山田暉 <hikaruy0805@gmail.com>
2018-09-18 08:35:31 +0900

    complete

--- chapter4/nlp34.py
+++ chapter4/nlp34.py
@@ -4,9 +4,16 @@
 import nlp30
 
 lines = nlp30.neko_lines()
-nouns = []
+nouns_a_no_b = []
 for line in lines:
-  for 
+  if len(line) > 2:
+    for i in range(1, len(line) - 1):
+      if line[i]['surface'] == 'の' \
+        and line[i-1]['pos'] == '名詞' \
+        and line[i+1]['pos'] == '名詞':
+        nouns_a_no_b.append(line[i-1]['surface']+'の'+line[i+1]['surface'])
+a_no_b = set(nouns_a_no_b)
 
 
-print(nouns)
+sorted_a_no_b = sorted(a_no_b, key=nouns_a_no_b.index)
+print(sorted_a_no_b)
