ec90b0feb3cac9131b63cf722a9c2ae68d87dda4
山田暉 <hikaruy0805@gmail.com>
2018-09-17 22:03:54 +0900

    もう一度行う Q31

--- chapter4/nlp31.py
+++ chapter4/nlp31.py
@@ -2,3 +2,14 @@
 # 動詞の表層形をすべて抽出せよ．
 
 import nlp30
+
+nlp30.parse_neko()
+
+lines = nlp30.neko_lines()
+verbs = []
+for line in lines:
+  for morpheme in line:
+    if morpheme['pos'] == '動詞':
+      verbs.append(morpheme['surface'])
+
+print(verbs)
