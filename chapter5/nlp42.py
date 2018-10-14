723ceca3f4c47d115d7c7f6e2cad539258f537ac
山田暉 <hikaruy0805@gmail.com>
2018-10-14 09:21:02 +0900

    add if __name__ ==  '__main__':

--- chapter5/nlp42.py
+++ chapter5/nlp42.py
@@ -7,15 +7,16 @@ from nlp41 import Chunk
 
 #nlp41のChunkクラスに関数normalized_surface()を追加
 
-nlp40.relate_neko()
-# 1文ずつリスト作成
-for chunks in nlp41.neko_lines():
+if __name__ == '__main__':
+  nlp40.relate_neko()
+  # 1文ずつリスト作成
+  for chunks in nlp41.neko_lines():
     for chunk in chunks:
-        if chunk.dst != -1:
-            src = chunk.normalized_surface()
-            dst = chunks[chunk.dst].normalized_surface()
-            if src != '' and dst != '':
-                print('{}\t{}'.format(src, dst))
+      if chunk.dst != -1:
+        src = chunk.normalized_surface()
+        dst = chunks[chunk.dst].normalized_surface()
+        if src != '' and dst != '':
+            print('{}\t{}'.format(src, dst))
 
 
 
